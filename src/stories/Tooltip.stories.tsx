c771c117702da492e86eae898f6c6aa185697db0
BADtochka <75701093+BADtochka@users.noreply.github.com>
2024-04-26 13:04:51 +0300

    [RPDEV-234] Компонент Тулы и типа (#15)
    * feat: add Slider component
    
    * feat: add Tooltip component
    
    * feat: add Icon props

---
+++ src/stories/Tooltip.stories.tsx
@@ -0,0 +1,30 @@
+import { Button } from '@components/Button';
+import { IconNames } from '@components/Icon';
+import { Tooltip } from '@components/Tooltip';
+import { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  component: Tooltip,
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    iconLeft: {
+      options: IconNames
+    },
+    iconRight: {
+      options: IconNames
+    }
+  }
+} as Meta<typeof Tooltip>;
+
+export const Preview: StoryObj<typeof Tooltip> = {
+  args: {
+    text: 'Tooltip text'
+  },
+  render: (props) => (
+    <Tooltip {...props}>
+      <Button dataSize='sm'>Tooltip</Button>
+    </Tooltip>
+)
+};
