3beae20abb8c1680ec9010f005efe221a0877a49
BADtochka <badmodertwitch@gmail.com>
2024-04-26 15:36:34 +0300

    feat: update Dropdown props

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,16 +1,21 @@
 import { Button } from '@components/Button'
-import { FC, PropsWithChildren } from 'react'
+import { FC, MouseEvent, PropsWithChildren } from 'react'
 import './style.scss'
 
 interface DropdownProps {
   open?: boolean
   label?: string
+  triggerSize: 'xs' | 'sm' | 'md'
+  onClick: (event: MouseEvent<HTMLButtonElement>) => void
 }
 
-export const Dropdown: FC<PropsWithChildren<DropdownProps>> = ({ label, open, children }) => {
+export const Dropdown: FC<PropsWithChildren<DropdownProps>> = ({ label, triggerSize = 'md', open, children }) => {
+  // TODO: onMouseLeave?
+  // TODO: on outer click close
+
   return (
     <div className="ev-dropdown" data-open={ open }>
-      <Button iconRight='TbCaretDownFilled'>
+      <Button iconRight='TbCaretDownFilled' size={ triggerSize }>
         { label }
       </Button>
       <div className="ev-dropdown-content">
