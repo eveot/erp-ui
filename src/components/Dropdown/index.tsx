6607c74ef8b0c476d3b2efc6a4360af010a245d2
BADtochka <badmodertwitch@gmail.com>
2024-05-01 13:43:37 +0300

    feat: update Dropdown props

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -6,18 +6,19 @@ import './style.scss'
 interface DropdownProps {
   open?: boolean
   label?: string
-  triggerSize: 'xs' | 'sm' | 'md'
+  triggerSize?: 'xs' | 'sm' | 'md'
+  triggerCentered?: boolean
   onOpen?: () => void
   onClose?: () => void
 }
 
-export const Dropdown: FC<PropsWithChildren<DropdownProps>> = ({ label, triggerSize = 'md', open, children, onOpen, onClose }) => {
+export const Dropdown: FC<PropsWithChildren<DropdownProps>> = ({ label, triggerSize, triggerCentered = false, open, children, onOpen, onClose }) => {
 
   const { ref } = useOuterClick<HTMLDivElement>(() => onClose && onClose())
 
   return (
     <div className="ev-dropdown" ref={ ref } data-open={ open } onClick={ open ? onClose : onOpen }>
-      <Button iconRight='TbCaretDownFilled' size={ triggerSize }>
+      <Button iconRight='TbCaretDownFilled' size={ triggerSize } centered={ triggerCentered }>
         { label }
       </Button>
       <div className="ev-dropdown-content">
