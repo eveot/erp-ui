2b80fea44115cc67c0257e280004d018318afa57
BADtochka <badmodertwitch@gmail.com>
2024-05-01 17:13:41 +0300

    fix: undefined Icon component

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,5 +1,5 @@
 import * as evicons from '@components/Icon/icons'
-import { FC } from 'react'
+import React, { FC } from 'react'
 import { IconBaseProps } from 'react-icons'
 import * as tbicons from 'react-icons/tb'
 import './style.scss'
@@ -18,8 +18,20 @@ export interface IconProps {
 export const IconNames = Object.keys(evicons).concat(Object.keys(tbicons))
 
 export const Icon: FC<IconProps> = ({ name, color = 'currentColor', size = '1.063rem', onClick }) => {
-  const ParsedIcon = Object.keys(evicons).includes(name) ? evicons[name as EveotIconName] : tbicons[name as TablerIconName]
-  
+
+  const getParsedIcon = (): React.ElementType | null => {
+    if (Object.keys(evicons).includes(name)) {
+      return evicons[name as EveotIconName];
+    } else {
+      return tbicons[name as TablerIconName];
+    }
+  };
+
+  const ParsedIcon = getParsedIcon();
+
+  if (!ParsedIcon) {
+    return <p style={{color: '#F44336'}}>ICON:ERR</p>;
+  }
   return (
     <div className="ev-icon" onClick={onClick}>
       <ParsedIcon size={size} color={color} />
