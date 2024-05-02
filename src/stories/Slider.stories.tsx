8d07c12f7069bfdb7903577923bf8cffb6f5ae20
BADtochka <badmodertwitch@gmail.com>
2024-05-02 17:27:59 +0300

    feat(Slider): add min & max props

--- src/stories/Slider.stories.tsx
+++ src/stories/Slider.stories.tsx
@@ -1,15 +1,11 @@
 import { Slider } from '@components/Slider';
 import { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
 
 export default {
   component: Slider,
   parameters: {
     layout: 'centered',
-  },
-  argTypes: {
-    value: {
-      control: 'range'
-    }
   },
   tags: ['autodocs'],
 } as Meta<typeof Slider>;
@@ -18,5 +14,16 @@ export const Default: StoryObj<typeof Slider> = {
   args: {
     label: 'Label',
     value: 30
+  },
+  render: (props) => {
+    const [value, setValue] = useState(0);
+
+    return (
+      <Slider
+        {...props}
+        value={value}
+        onChange={(event) => setValue(Number(event.currentTarget.value))}
+      />
+    );
   }
 };
