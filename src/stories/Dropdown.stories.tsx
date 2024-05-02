594cdfa305e8ab7d83790daf83c7b7f96cfd6ada
BADtochka <badmodertwitch@gmail.com>
2024-05-02 12:41:49 +0300

    fix: dropdown rotate icons in content

--- src/stories/Dropdown.stories.tsx
+++ src/stories/Dropdown.stories.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@components/Button';
 import { Dropdown } from '@components/Dropdown';
+import { IconNames } from '@components/Icon';
 import { Meta, StoryObj } from '@storybook/react';
 import { MouseEvent, useState } from 'react';
 
@@ -8,12 +9,16 @@ export default {
   parameters: {
     layout: 'centered',
   },
+  argTypes: {
+    triggerIcon: {
+      options: IconNames
+    }
+  },
   tags: ['autodocs'],
 } as Meta<typeof Dropdown>;
 
 
 export const Default: StoryObj<typeof Dropdown> = {
-
   args: {
     label: 'Dropdown Text'
   },
@@ -26,10 +31,10 @@ export const Default: StoryObj<typeof Dropdown> = {
 
   return (
     <Dropdown {...props} open={ test } onOpen={() => setTest(true)} onClose={() => setTest(false)}>
-      <Button name='1' onClick={ onButtonClick }>1</Button>
-      <Button name='2' onClick={ onButtonClick }>2</Button>
-      <Button name='3' onClick={ onButtonClick }>3</Button>
-      <Button name='4' onClick={ onButtonClick }>4</Button>
+      <Button name='1' iconLeft='TbCellSignal3' onClick={ onButtonClick }>1</Button>
+      <Button name='2' iconLeft='TbAccessPointOff' onClick={ onButtonClick }>2</Button>
+      <Button name='3' iconLeft='TbAirBalloon' onClick={ onButtonClick }>3</Button>
+      <Button name='4' iconLeft='EvFlagPL' onClick={ onButtonClick }>4</Button>
     </Dropdown>
   )
 }
