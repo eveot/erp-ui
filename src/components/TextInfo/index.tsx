6e552afc804acc6df6ea0e350f9f93e6739a26e5
BADtochka <badmodertwitch@gmail.com>
2024-05-07 19:36:14 +0300

    fix: components
    - [x]  TextInfo при disabled работает active и целом, onClick
    - [x]  Button width 100% - выпилить
    - [x]  Button active не срабатывает при клике
    - [x]  Input при фокусе на wrapper должен переводить курсор в конец
    - [x]  Input на всю ширину wrapper’а

--- src/components/TextInfo/index.tsx
+++ src/components/TextInfo/index.tsx
@@ -7,18 +7,19 @@ export interface TextInfoProps {
   disabled?: boolean
   iconLeft?: IconName
   iconRight?: IconName
+  onClick?: () => void
 }
 
-export type TextInfo = {
-  label: string
-  iconLeft?: IconName
-  iconRight?: IconName
-}
+export const TextInfo: FC<PropsWithChildren<TextInfoProps>> = ({ active, disabled, iconLeft, iconRight, children, onClick }) => {
 
-export const TextInfo: FC<PropsWithChildren<TextInfoProps>> = ({ active, disabled, iconLeft, iconRight, children }) => {
+  const _onClick = () => {
+    if (onClick && !disabled) {
+      onClick()
+    }
+  }
 
   return (
-    <div className="ev-text-info" data-active={active} data-disabled={disabled}>
+    <div className="ev-text-info" data-active={active} data-disabled={disabled} onClick={ _onClick }>
       { iconLeft &&  <Icon name={ iconLeft } /> }
       <p>{ children }</p>
       { iconRight && <Icon name={ iconRight } /> }
